28e9d498972ade2fe5c72309f9fb698d355c851a
xnivel <xnivel@gmail.com>
2015-04-10 01:29:44 +0200

    serwer

--- server/main.py
+++ server/main.py
@@ -5,6 +5,8 @@ import tornado.web
 import pymongo
 # json
 import json
+import re
+import datetime
 
 conncection = pymongo.Connection('localhost')
 db=conncection['pharmapp']
@@ -14,14 +16,32 @@ class MainHandler(tornado.web.RequestHandler):
         self.write("Hello, world")
 
 class Statistic(tornado.web.RequestHandler):
-    def getrestult(self,a):
+    def add_month(self,source):
+        year = source.year + int(source.month/12)
+        month = source.month %12 +1
+        return datetime.datetime(year,month,1)
+    def getrestultatdate(self,date):
         results = []
-        pipeline =[{"$group":{ "_id": "$name","cena": {"$sum": {"$multiply": ["$price","$count"]}}}}] 
+        pipeline =[{"$match": {"$date":{"$gte": date,"$lt": self.add_month(date)}}},{"$group":{ "_id": "$name","count": {"$sum": "$count"},"price": {"$sum": {"$multiply": ["$price","$count"]}}}}] 
         for result in db.past.aggregate(pipeline)["result"]:
+            result['name']=result['_id']
             results.append(result)
         return results
-    def get(self,name):
-        self.write(json.dumps(self.getrestult(name)))
+    def getrestult(self):
+        results = []
+        pipeline =[{"$group":{ "_id": "$name","count": {"$sum": "$count"},"price": {"$sum": {"$multiply": ["$price","$count"]}}}}] 
+        for result in db.past.aggregate(pipeline)["result"]:
+            result['name']=result['_id']
+            results.append(result)
+        return results
+    def get(self,date):
+        if re.match("[01][0-9]-[12][0-9]{3}",date):
+            parsedDate=datetime.datetime(int(date[4:7]),int(date[0:2]),1)
+            parsed2=self.add_month(parsedDate)
+            
+            self.write(json.dumps(self.getrestult(parsedDate=datetime.datetime(int(date[4:7]),int(date[0:2]),1))))
+        else:
+            self.write(json.dumps(self.getrestult()))
     
 class SearchDrugs(tornado.web.RequestHandler):
     def getrestult(self,name):
@@ -36,7 +56,7 @@ class SearchDrugs(tornado.web.RequestHandler):
 application = tornado.web.Application([
     (r"/", MainHandler),
     (r"/search/([a-zA-Z ]*)", SearchDrugs),
-    (r"/statisticFrom/([a-zA-Z ]*)", Statistic),
+    (r"/statisticFrom/([- 0-9a-zA-Z]*)", Statistic),
 ])
 
 if __name__ == "__main__":
