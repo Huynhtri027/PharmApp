4d95d9d25bd42cb9c96478ea7d8e52b62de1d702
xnivel <xnivel@gmail.com>
2015-04-12 21:44:39 +0200

    server rc1

--- server/main.py
+++ server/main.py
@@ -22,7 +22,8 @@ class Statistic(tornado.web.RequestHandler):
         return datetime.datetime(year,month,1)
     def getrestultatdate(self,date):
         results = []
-        pipeline =[{"$match": {"$date":{"$gte": date,"$lt": self.add_month(date)}}},{"$group":{ "_id": "$name","count": {"$sum": "$count"},"price": {"$sum": {"$multiply": ["$price","$count"]}}}}] 
+        pipeline =[{"$match": {"date":{"$gte": date,"$lte": self.add_month(date)}}},{"$group":{ "_id": "$name","count": {"$sum": "$count"},"price": {"$sum": {"$multiply": ["$price","$count"]}}}}] 
+#        pipeline =[{"$match": {"date":{"$gte": self.add_month(date)}}},{"$group":{ "_id": "$name","count": {"$sum": "$count"},"price": {"$sum": {"$multiply": ["$price","$count"]}}}}] 
         for result in db.past.aggregate(pipeline)["result"]:
             result['name']=result['_id']
             results.append(result)
@@ -36,12 +37,40 @@ class Statistic(tornado.web.RequestHandler):
         return results
     def get(self,date):
         if re.match("[01][0-9]-[12][0-9]{3}",date):
-            parsedDate=datetime.datetime(int(date[4:7]),int(date[0:2]),1)
+            parsedDate=datetime.datetime(int(date[3:7]),int(date[0:2]),1)
             parsed2=self.add_month(parsedDate)
-            
-            self.write(json.dumps(self.getrestult(parsedDate=datetime.datetime(int(date[4:7]),int(date[0:2]),1))))
+            self.write(json.dumps(self.getrestultatdate(datetime.datetime(int(date[3:7]),int(date[0:2]),1))))
         else:
             self.write(json.dumps(self.getrestult()))
+class PurchasedDrugs(tornado.web.RequestHandler):
+    def updatedrug(self,js):
+        db.drugs.update({"name": js['name']},{"$inc": {"count": -1*js['count']}})
+        db.past.insert({"name":js['name'],"count":js['count'],"price":js['price'],"date":datetime.datetime.now()})
+    def checdrug(self,jsondr,array):
+        dbcontain= db.drugs.find_one({ "name": jsondr['name']} )
+        if(dbcontain['count']<jsondr['count']):
+            dbcontain['_id']=str(dbcontain['_id'])
+            array.append(dbcontain)
+    def getpostresult(self,jsonarray):
+        result=[]
+        for js in jsonarray:
+            self.checdrug(js,result)
+        if(len(result)>0):
+            #print("braki")
+            return result
+        else:
+            #print("ok")
+            for js in jsonarray:
+                self.updatedrug(js)
+        return result
+    def post(self):
+        data_json= tornado.escape.json_decode(self.request.body)
+#        self.write(o"asd")
+        result=self.getpostresult(data_json)
+        #print(self.request.body)
+        #print(result)
+        self.write(json.dumps(result))
+#        self.write(data_json)
     
 class SearchDrugs(tornado.web.RequestHandler):
     def getrestult(self,name):
@@ -55,6 +84,7 @@ class SearchDrugs(tornado.web.RequestHandler):
 
 application = tornado.web.Application([
     (r"/", MainHandler),
+    (r"/buyDrug", PurchasedDrugs),
     (r"/search/([a-zA-Z ]*)", SearchDrugs),
     (r"/statisticFrom/([- 0-9a-zA-Z]*)", Statistic),
 ])
